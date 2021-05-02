c4df076cc2bc545f5a3e0146498a190d8a515c25
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-05-02 15:25:48 -0400

    Following a Tutorial: Paint App
    https://medium.com/@nidhinp.412/paint-app-in-django-a54371044624

--- draw/views.py
+++ draw/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the draw index.")
+    return render(request, 'draw.html')
 
 
